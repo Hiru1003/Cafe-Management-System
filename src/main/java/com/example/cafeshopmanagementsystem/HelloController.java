d85814bd44b34800e8ba0c09a391088d11014c2d
Hiru1003 <hirumiperera10@gmail.com>
2024-03-31 19:10:17 +0530

    feat: slider function

--- src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
+++ src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
@@ -3,11 +3,15 @@ package com.example.cafeshopmanagementsystem;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
 public class HelloController {
+
     @FXML
     private Hyperlink si_forgotPw;
 
@@ -24,6 +28,9 @@ public class HelloController {
     private TextField si_username;
 
     @FXML
+    private Button side_alreadyHave;
+
+    @FXML
     private Button side_createBtn;
 
     @FXML
@@ -36,9 +43,6 @@ public class HelloController {
     private PasswordField su_password;
 
     @FXML
-    private ComboBox<?> su_question;
-
-    @FXML
     private Button su_signupBtn;
 
     @FXML
@@ -47,38 +51,30 @@ public class HelloController {
     @FXML
     private TextField su_username;
 
-    @FXML
-    private Button side_alreadyHave;
 
-    public void switchForm(ActionEvent event){
+    public void switchForm(ActionEvent event) {
         TranslateTransition slider = new TranslateTransition();
+
         if (event.getSource() == side_createBtn) {
             slider.setNode(side_form);
+            slider.setDuration(Duration.seconds(0.5));
             slider.setToX(300);
-            slider.setDuration(Duration.seconds(.5));
 
             slider.setOnFinished((ActionEvent e) -> {
                 side_alreadyHave.setVisible(true);
                 side_createBtn.setVisible(false);
             });
-
             slider.play();
-        }
-        else if (event.getSource() == side_alreadyHave) {
+        } else if (event.getSource() == side_alreadyHave) {
             slider.setNode(side_form);
+            slider.setDuration(Duration.seconds(0.5));
             slider.setToX(0);
-            slider.setDuration(Duration.seconds(.5));
-
             slider.setOnFinished((ActionEvent e) -> {
                 side_alreadyHave.setVisible(false);
                 side_createBtn.setVisible(true);
             });
-            slider.play();
         }
 
+        slider.play();
     }
-
-
-
-
 }
