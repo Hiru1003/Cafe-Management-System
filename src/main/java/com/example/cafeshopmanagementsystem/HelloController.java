95f5f91c11d6d202db70ff54d2ba768d7c78db1f
Hiru1003 <hirumiperera10@gmail.com>
2024-03-31 10:11:09 +0530

    feat: contollers

--- src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
+++ src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
@@ -1,14 +1,56 @@
 package com.example.cafeshopmanagementsystem;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Hyperlink si_forgotPw;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button si_loginBtn;
+
+    @FXML
+    private AnchorPane si_loginform;
+
+    @FXML
+    private PasswordField si_passwords;
+
+    @FXML
+    private TextField si_username;
+
+    @FXML
+    private Button side_createBtn;
+
+    @FXML
+    private AnchorPane side_form;
+
+    @FXML
+    private TextField su_asnwer;
+
+    @FXML
+    private PasswordField su_password;
+
+    @FXML
+    private ComboBox<?> su_question;
+
+    @FXML
+    private Button su_signupBtn;
+
+    @FXML
+    private AnchorPane su_signupForm;
+
+    @FXML
+    private TextField su_username;
+
+    @FXML
+    private Button side_alreadyHave;
+
+    public void switchForm(){
+
     }
+
+
+
 }
