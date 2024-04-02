169989040951f18d77db0c7f01e9971d28b2c413
Hiru1003 <hirumiperera10@gmail.com>
2024-04-02 09:32:33 +0530

    feat: regbtn else caluse

--- src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
+++ src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
@@ -80,7 +80,8 @@ public class HelloController {
     private ResultSet result;
     private Alert alert;
 
-    public void regBtn(){
+    public void regBtn() {
+
         if (su_username.getText().isEmpty() || su_password.getText().isEmpty()
                 || su_question.getSelectionModel().getSelectedItem() == null
                 || su_asnwer.getText().isEmpty()) {
@@ -89,7 +90,74 @@ public class HelloController {
             alert.setHeaderText(null);
             alert.setContentText("Please fill all blank fields");
             alert.showAndWait();
+        } else {
+
+            String regData = "INSERT INTO employee (username, password, question, answer, date) "
+                    + "VALUES(?,?,?,?,?)";
+            connect = database.connectDB();
+
+            try {
+                // CHECK IF THE USERNAME IS ALREADY RECORDED
+                String checkUsername = "SELECT username FROM employee WHERE username = '"
+                        + su_username.getText() + "'";
+
+                prepare = connect.prepareStatement(checkUsername);
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText(su_username.getText() + " is already taken");
+                    alert.showAndWait();
+                } else if (su_password.getText().length() < 8) {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Invalid Password, atleast 8 characters are needed");
+                    alert.showAndWait();
+                } else {
+                    prepare = connect.prepareStatement(regData);
+                    prepare.setString(1, su_username.getText());
+                    prepare.setString(2, su_password.getText());
+                    prepare.setString(3, (String) su_question.getSelectionModel().getSelectedItem());
+                    prepare.setString(4, su_asnwer.getText());
+
+                    Date date = new Date();
+                    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+                    prepare.setString(5, String.valueOf(sqlDate));
+                    prepare.executeUpdate();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully registered Account!");
+                    alert.showAndWait();
+
+                    su_username.setText("");
+                    su_password.setText("");
+                    su_question.getSelectionModel().clearSelection();
+                    su_asnwer.setText("");
+
+                    TranslateTransition slider = new TranslateTransition();
+
+                    slider.setNode(side_form);
+                    slider.setToX(0);
+                    slider.setDuration(Duration.seconds(.5));
+
+                    slider.setOnFinished((ActionEvent e) -> {
+                        side_alreadyHave.setVisible(false);
+                        side_createBtn.setVisible(true);
+                    });
+
+                    slider.play();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
 
