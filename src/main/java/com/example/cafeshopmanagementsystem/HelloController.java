32072a52be08ee209dfee0cbd0ac27b790773909
Hiru1003 <hirumiperera10@gmail.com>
2024-04-03 07:54:30 +0530

    feat: login btn

--- src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
+++ src/main/java/com/example/cafeshopmanagementsystem/HelloController.java
@@ -80,6 +80,69 @@ public class HelloController {
     private ResultSet result;
     private Alert alert;
 
+    public void loginBtn() {
+
+        if (si_username.getText().isEmpty() || si_passwords.getText().isEmpty()) {
+            alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Incorrect Username/Password");
+            alert.showAndWait();
+        } else {
+
+            String selctData = "SELECT username, password FROM employee WHERE username = ? and password = ?";
+
+            connect = database.connectDB();
+
+            try {
+
+                prepare = connect.prepareStatement(selctData);
+                prepare.setString(1, si_username.getText());
+                prepare.setString(2, si_passwords.getText());
+
+                result = prepare.executeQuery();
+                // IF SUCCESSFULLY LOGIN, THEN PROCEED TO ANOTHER FORM WHICH IS OUR MAIN FORM
+                if (result.next()) {
+                    // TO GET THE USERNAME THAT USER USED
+                    data.username = si_username.getText();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+
+                    // LINK YOUR MAIN FORM
+                    Parent root = FXMLLoader.load(getClass().getResource("mainForm.fxml"));
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    stage.setTitle("Cafe Shop Management System");
+                    stage.setMinWidth(1100);
+                    stage.setMinHeight(600);
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                    si_loginBtn.getScene().getWindow().hide();
+
+                } else { // IF NOT, THEN THE ERROR MESSAGE WILL APPEAR
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Incorrect Username/Password");
+                    alert.showAndWait();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
     public void regBtn() {
 
         if (su_username.getText().isEmpty() || su_password.getText().isEmpty()
