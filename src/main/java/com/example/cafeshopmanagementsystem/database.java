ffe507f7a4a40bcc0082835f00f9b8a96285d546
Hiru1003 <hirumiperera10@gmail.com>
2024-04-07 12:56:30 +0530

    feat: db connection done

--- src/main/java/com/example/cafeshopmanagementsystem/database.java
+++ src/main/java/com/example/cafeshopmanagementsystem/database.java
@@ -2,18 +2,24 @@ package com.example.cafeshopmanagementsystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class database {
 
     public static Connection connectDB() {
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
 
-            Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Mocha", "root", "Hiru1003*"); // LINK YOUR DATABASE
+            String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/Mocha";
+            String username = "root";
+            String password = "Hiru1003*";
+
+            Connection connect = DriverManager.getConnection(jdbcUrl, username, password); // LINK YOUR DATABASE
+            System.out.println("Database connection successful.");
             return connect;
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database: " + e.getMessage());
         }
         return null;
     }}
