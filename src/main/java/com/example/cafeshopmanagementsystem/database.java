1d8b12a7b254e9384608a6a4547478a8c334431e
Hiru1003 <hirumiperera10@gmail.com>
2024-04-07 10:14:07 +0530

    feat: new db connection

--- src/main/java/com/example/cafeshopmanagementsystem/database.java
+++ src/main/java/com/example/cafeshopmanagementsystem/database.java
@@ -2,21 +2,23 @@ package com.example.cafeshopmanagementsystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class database {
 
     public static Connection connectDB() {
 
+        Connection connect = null;
         try {
-
+            // Load MySQL JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
 
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/cafemocha", "root", ""); // LINK YOUR DATABASE
-            return connect;
-        } catch (Exception e) {
-            e.printStackTrace();
+            // Establish connection to the database
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/Mocha", "root", "Hiru1003*");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not load database driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
         }
-        return null;
-    }
-
-}
+        return connect;
+    }}
