ae3202bce0ef610e2a20cf8ddbea1af0ad4c6a9a
Hiru1003 <hirumiperera10@gmail.com>
2024-04-07 12:28:37 +0530

    feat: my sql connection to cafe management system

--- src/main/java/com/example/cafeshopmanagementsystem/database.java
+++ src/main/java/com/example/cafeshopmanagementsystem/database.java
@@ -2,23 +2,18 @@ package com.example.cafeshopmanagementsystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class database {
 
     public static Connection connectDB() {
 
-        Connection connect = null;
         try {
-            // Load MySQL JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
 
-            // Establish connection to the database
-            connect = DriverManager.getConnection("jdbc:mysql://localhost/Mocha", "root", "Hiru1003*");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Could not load database driver: " + e.getMessage());
-        } catch (SQLException e) {
-            System.err.println("Error connecting to the database: " + e.getMessage());
+            Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Mocha", "root", "Hiru1003*"); // LINK YOUR DATABASE
+            return connect;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return connect;
+        return null;
     }}
