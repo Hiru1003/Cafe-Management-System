9625d5b0ec342eb94f90c8fbab2b27e78403ff73
Hiru1003 <hirumiperera10@gmail.com>
2024-04-08 10:44:28 +0530

    feat: add delete butons

--- src/main/java/com/example/cafeshopmanagementsystem/MainForm.java
+++ src/main/java/com/example/cafeshopmanagementsystem/MainForm.java
@@ -60,25 +60,25 @@ public class MainForm implements Initializable  {
     private Button inventory_clearBtn;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_date;
+    private TableColumn<productData, String> inventory_col_date;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_price;
+    private TableColumn<productData, String> inventory_col_price;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_prodcutId;
+    private TableColumn<productData, String> inventory_col_prodcutId;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_productName;
+    private TableColumn<productData, String> inventory_col_productName;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_status;
+    private TableColumn<productData, String> inventory_col_status;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_stoke;
+    private TableColumn<productData, String> inventory_col_stoke;
 
     @FXML
-    private TableColumn<?, ?> inventory_col_type;
+    private TableColumn<productData, String> inventory_col_type;
 
     @FXML
     private Button inventory_deleteBtn;
@@ -108,7 +108,7 @@ public class MainForm implements Initializable  {
     private TextField inventory_stock;
 
     @FXML
-    private TableView<?> inventory_tableView;
+    private TableView<productData> inventory_tableView;
 
     @FXML
     private ComboBox<?> inventory_type;
@@ -138,6 +138,310 @@ public class MainForm implements Initializable  {
     private ResultSet result;
 
     private Image image;
+
+
+    private ObservableList<productData> cardListData = FXCollections.observableArrayList();
+
+    public void inventoryAddBtn() {
+
+        if (inventory_productId.getText().isEmpty()
+                || inventory_productName.getText().isEmpty()
+                || inventory_type.getSelectionModel().getSelectedItem() == null
+                || inventory_stock.getText().isEmpty()
+                || inventory_price.getText().isEmpty()
+                || inventory_status.getSelectionModel().getSelectedItem() == null
+                || data.path == null) {
+
+            alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all blank fields");
+            alert.showAndWait();
+
+        } else {
+
+            // CHECK PRODUCT ID
+            String checkProdID = "SELECT prod_id FROM product WHERE prod_id = '"
+                    + inventory_productId.getText() + "'";
+
+            connect = database.connectDB();
+
+            try {
+
+                statement = connect.createStatement();
+                result = statement.executeQuery(checkProdID);
+
+                if (result.next()) {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText(inventory_productId.getText() + " is already taken");
+                    alert.showAndWait();
+                } else {
+                    String insertData = "INSERT INTO product "
+                            + "(prod_id, prod_name, type, stock, price, status, image, date) "
+                            + "VALUES(?,?,?,?,?,?,?,?)";
+
+                    prepare = connect.prepareStatement(insertData);
+                    prepare.setString(1, inventory_productId.getText());
+                    prepare.setString(2, inventory_productName.getText());
+                    prepare.setString(3, (String) inventory_type.getSelectionModel().getSelectedItem());
+                    prepare.setString(4, inventory_stock.getText());
+                    prepare.setString(5, inventory_price.getText());
+                    prepare.setString(6, (String) inventory_status.getSelectionModel().getSelectedItem());
+
+                    String path = data.path;
+                    path = path.replace("\\", "\\\\");
+
+                    prepare.setString(7, path);
+
+                    // TO GET CURRENT DATE
+                    Date date = new Date();
+                    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+
+                    prepare.setString(8, String.valueOf(sqlDate));
+
+                    prepare.executeUpdate();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Added!");
+                    alert.showAndWait();
+
+                    inventoryShowData();
+                    inventoryClearBtn();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void inventoryUpdateBtn() {
+
+        if (inventory_productId.getText().isEmpty()
+                || inventory_productName.getText().isEmpty()
+                || inventory_type.getSelectionModel().getSelectedItem() == null
+                || inventory_stock.getText().isEmpty()
+                || inventory_price.getText().isEmpty()
+                || inventory_status.getSelectionModel().getSelectedItem() == null
+                || data.path == null || data.id == 0) {
+
+            alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all blank fields");
+            alert.showAndWait();
+
+        } else {
+
+            String path = data.path;
+            path = path.replace("\\", "\\\\");
+
+            String updateData = "UPDATE product SET "
+                    + "prod_id = '" + inventory_productId.getText() + "', prod_name = '"
+                    + inventory_productName.getText() + "', type = '"
+                    + inventory_type.getSelectionModel().getSelectedItem() + "', stock = '"
+                    + inventory_stock.getText() + "', price = '"
+                    + inventory_price.getText() + "', status = '"
+                    + inventory_status.getSelectionModel().getSelectedItem() + "', image = '"
+                    + path + "', date = '"
+                    + data.date + "' WHERE id = " + data.id;
+
+            connect = database.connectDB();
+
+            try {
+
+                alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Are you sure you want to UPDATE PRoduct ID: " + inventory_productId.getText() + "?");
+                Optional<ButtonType> option = alert.showAndWait();
+
+                if (option.get().equals(ButtonType.OK)) {
+                    prepare = connect.prepareStatement(updateData);
+                    prepare.executeUpdate();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Updated!");
+                    alert.showAndWait();
+
+                    // TO UPDATE YOUR TABLE VIEW
+                    inventoryShowData();
+                    // TO CLEAR YOUR FIELDS
+                    inventoryClearBtn();
+                } else {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Cancelled.");
+                    alert.showAndWait();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void inventoryDeleteBtn() {
+        if (data.id == 0) {
+
+            alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all blank fields");
+            alert.showAndWait();
+
+        } else {
+            alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to DELETE Product ID: " + inventory_productId.getText() + "?");
+            Optional<ButtonType> option = alert.showAndWait();
+
+            if (option.get().equals(ButtonType.OK)) {
+                String deleteData = "DELETE FROM product WHERE id = " + data.id;
+                try {
+                    prepare = connect.prepareStatement(deleteData);
+                    prepare.executeUpdate();
+
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("successfully Deleted!");
+                    alert.showAndWait();
+
+                    // TO UPDATE YOUR TABLE VIEW
+                    inventoryShowData();
+                    // TO CLEAR YOUR FIELDS
+                    inventoryClearBtn();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Cancelled");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    public void inventoryClearBtn() {
+
+        inventory_productId.setText("");
+        inventory_productName.setText("");
+        inventory_type.getSelectionModel().clearSelection();
+        inventory_stock.setText("");
+        inventory_price.setText("");
+        inventory_status.getSelectionModel().clearSelection();
+        data.path = "";
+        data.id = 0;
+        inventory_imageView.setImage(null);
+
+    }
+
+
+    public void inventoryImportBtn() {
+
+        FileChooser openFile = new FileChooser();
+        openFile.getExtensionFilters().add(new ExtensionFilter("Open Image File", "*.png", "*.jpg"));
+
+        File file = openFile.showOpenDialog(main_Form.getScene().getWindow());
+
+        if (file != null) {
+
+            data.path = file.getAbsolutePath();
+            image = new Image(file.toURI().toString(), 124, 128, false, true);
+
+            inventory_imageView.setImage(image);
+        }
+    }
+
+    public void inventorySelectData() {
+
+        productData prodData = inventory_tableView.getSelectionModel().getSelectedItem();
+        int num = inventory_tableView.getSelectionModel().getSelectedIndex();
+
+        if ((num - 1) < -1) {
+            return;
+        }
+
+        inventory_productId.setText(prodData.getProductId());
+        inventory_productName.setText(prodData.getProductName());
+        inventory_stock.setText(String.valueOf(prodData.getStock()));
+        inventory_price.setText(String.valueOf(prodData.getPrice()));
+
+        data.path = prodData.getImage();
+
+        String path = "File:" + prodData.getImage();
+        data.date = String.valueOf(prodData.getDate());
+        data.id = prodData.getId();
+
+        image = new Image(path, 124, 128, false, true);
+        inventory_imageView.setImage(image);
+    }
+
+    public ObservableList<productData> inventoryDataList() {
+
+        ObservableList<productData> listData = FXCollections.observableArrayList();
+
+        String sql = "SELECT * FROM product";
+
+        connect = database.connectDB();
+
+        try {
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            productData prodData;
+
+            while (result.next()) {
+
+                prodData = new productData(result.getInt("id"),
+                        result.getString("prod_id"),
+                        result.getString("prod_name"),
+                        result.getString("type"),
+                        result.getInt("stock"),
+                        result.getDouble("price"),
+                        result.getString("status"),
+                        result.getString("image"),
+                        result.getDate("date"));
+
+                listData.add(prodData);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listData;
+    }
+
+    private ObservableList<productData> inventoryListData;
+
+    public void inventoryShowData() {
+        inventoryListData = inventoryDataList();
+
+        inventory_col_prodcutId.setCellValueFactory(new PropertyValueFactory<>("productId"));
+        inventory_col_productName.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        inventory_col_type.setCellValueFactory(new PropertyValueFactory<>("type"));
+        inventory_col_stoke.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        inventory_col_price.setCellValueFactory(new PropertyValueFactory<>("price"));
+        inventory_col_status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        inventory_col_date.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        inventory_tableView.setItems(inventoryListData);
+
+    }
+
 
     private String[] typeList = {"Meals", "Drinks"};
 
@@ -235,6 +539,7 @@ public class MainForm implements Initializable  {
         displayUsername();
         inventoryTypeList();
         inventoryStatusList();
+        inventoryShowData();
 
     }
 
